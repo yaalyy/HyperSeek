472331b4f712089fd5e6bb1eab96e16d79f52963
yaalyy <yaalyy@outlook.com>
2025-02-13 18:17:27 +0000

    updated param

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
   print("Hi, I'm DeepSeek. How can I help you today?")
   while(1):
     user_input = input()
-    time.sleep(random.randint(3, 10))
+    time.sleep(random.randint(3, 6))
     print("The server is busy. Please try again later.")
+
